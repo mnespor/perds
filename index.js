40d891c69a6a155591ed5e386816e77df065ee1b
mnespor <mnespor@gmail.com>
2018-08-16 06:40:48 -0500

    feat: Stubs for index and freeze

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const freeze = require('./lib/freeze')
+const getIn = require('./lib/get-in')
+const merge = require('./lib/merge')
+const mergeWith = require('./lib/merge')
+
+module.exports = {
+    freeze,
+    getIn,
+    merge,
+    mergeWith
+}
