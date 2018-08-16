b4e592e1b3a83120ab8db51d8638fd2a072e4bed
mnespor <mnespor@gmail.com>
2018-08-16 06:50:00 -0500

    style: Run prettier
    - add lockfile
    - add to-do list

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-'use strict'
+"use strict";
 
-const freeze = require('./lib/freeze')
-const getIn = require('./lib/get-in')
-const merge = require('./lib/merge')
-const mergeWith = require('./lib/merge')
+const freeze = require("./lib/freeze");
+const getIn = require("./lib/get-in");
+const merge = require("./lib/merge");
+const mergeWith = require("./lib/merge");
 
 module.exports = {
-    freeze,
-    getIn,
-    merge,
-    mergeWith
-}
+  freeze,
+  getIn,
+  merge,
+  mergeWith
+};
