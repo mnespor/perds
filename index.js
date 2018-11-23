1b2cc1f36c29ce4f2ba5bb205b737c8b6ebbfecf
mnespor <mnespor@gmail.com>
2018-11-23 17:38:55 -0600

    refactor: Simplify frozen()
    - Make a list or map out of recursively frozen children
    - Extract proxying function into its own file (for use by set, setIn, get, and so on)

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const frozen = require("./lib/frozen");
 const getIn = require("./lib/get-in");
 const merge = require("./lib/merge");
 const mergeWith = require("./lib/merge");
+const setIn = require('./lib/set-in')
+const updateIn = require('./lib/update-in')
 
 module.exports = {
   frozen,
   getIn,
   merge,
-  mergeWith
+  mergeWith,
+  setIn,
+  updateIn
 };
